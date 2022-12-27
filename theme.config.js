1dd0628d64e6a540bf30269ded9f42f0bdf41d5a
shaunkh <shaun.khundker@gmail.com>
2022-12-27 08:33:01 -0500

    cleanup

--- theme.config.js
+++ theme.config.js
@@ -1,21 +1,43 @@
+import {
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiOutlineInstagram
+} from 'react-icons/ai'
+import { HiOutlineMail } from 'react-icons/hi'
 const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Shaun Khundker.
-      <a href="/feed.xml">RSS</a>
-      <style jsx>{`
-        a {
-          float: right;
-        }
-        @media screen and (max-width: 480px) {
-          article {
-            padding-top: 2rem;
-            padding-bottom: 4rem;
+      <div style={{ display: 'block' }}>
+        <time>{YEAR}</time> © Shaun Khundker.
+        <a href="/feed.xml">RSS</a>
+        <style jsx>{`
+          a {
+            float: right;
           }
-        }
-      `}</style>
+          @media screen and (max-width: 480px) {
+            article {
+              padding-top: 2rem;
+              padding-bottom: 4rem;
+            }
+          }
+        `}</style>
+        <div style={{ display: 'flex' }}>
+          <a href="https://twitter.com/shaunkhundker">
+            <AiOutlineTwitter size={30} color="black" />
+          </a>
+          <a href="https://instagram.com/shaunkhundker">
+            <AiOutlineInstagram size={30} color="black" />
+          </a>
+          <a href="https://github.com/shaunkh">
+            <AiOutlineGithub size={30} color="black" />
+          </a>
+          <a href="mailto:shaun.khundker@gmail.com">
+            <HiOutlineMail size={30} color="black" />
+          </a>
+        </div>
+      </div>
     </small>
   )
 }
